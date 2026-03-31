5b37d25bfc315b6a7539db99f9d366a7be9dd5ce
LUKE\lukel <lukelousg@gmail.com>
2026-03-31 16:35:24 +0800

    Meat JavaDoc

--- src/main/java/seedu/duke/model/items/Meat.java
+++ src/main/java/seedu/duke/model/items/Meat.java
@@ -19,7 +19,7 @@ public class Meat extends Item {
      * @param expiryDate Expiry date.
      * @param meatType Type of meat.
      * @param origin Origin of the meat.
-     *
+     * @param isFrozen check the frozen state of meat
      */
     public Meat(String name, int quantity, String binLocation,
                 String expiryDate, String meatType,
@@ -30,26 +30,41 @@ public class Meat extends Item {
         this.isFrozen = isFrozen;
     }
 
+    /** @return Origin of the meat. */
     public String getOrigin() {
         return origin;
     }
 
+    /** @param origin New origin value. */
     public void setOrigin(String origin) {
         this.origin = origin;
     }
 
+    /** @return Type of meat. */
     public String getMeatType() {
         return meatType;
     }
 
+    /** @param meatType New meat type. */
     public void setMeatType(String meatType) {
         this.meatType = meatType;
     }
 
+    /**
+     * Returns whether this item is frozen.
+     *
+     * @return {@code true} if the item is frozen, {@code false} otherwise.
+     */
     public boolean isFrozen() {
         return isFrozen;
     }
 
+    /**
+     * Converts this meat item into a storage-friendly string format.
+     *
+     * @param categoryName Name of the category this item belongs to.
+     * @return Storage string representation.
+     */
     @Override
     public String toStorageString(String categoryName) {
         return super.toStorageString(categoryName)
@@ -58,6 +73,11 @@ public class Meat extends Item {
                 + " isFrozen/" + isFrozen;
     }
 
+    /**
+     * Returns a string representation of this meat item.
+     *
+     * @return Formatted meat details.
+     */
     @Override
     public String toString() {
         return "[Meat] " + super.toString()
